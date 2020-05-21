6a2bb1e3ba15d551eacb46f92feb23a6f8fc6618
Amit Gupta <amit@ipc.iisc.ernet.in>
2020-05-21 12:44:35 +0530

    Undirected graphs update graph API

--- Graph.py
+++ Graph.py
@@ -12,6 +12,7 @@ class Graph():
             self.V = self.filobject.readInt()
             self.E = self.filobject.readInt()
             self.adj = [[] for _ in range(self.V)]
+            # instead of bags or linked lists or sets, adj is simple list of lists
             for _ in range(self.V):
                 v= self.filobject.readInt()
                 w= self.filobject.readInt()
@@ -22,6 +23,7 @@ class Graph():
 
     def addEdge(self,v,w):
         self.E+=1
+        #  instead of adding to bag, append to list 
         self.adj[v].append(w)
         self.adj[w].append(v)
 
@@ -34,3 +36,30 @@ class Graph():
     def toString(self):
         pass
 
+ # #######################################################################
+ # Graph processing API
+ # #######################################################################
+def degree(G,v):
+    degree = 0
+    for node in G.adj[v]:
+        degree+=1
+    return degree
+
+def maxDegree(G):
+    maxDegree = 0
+    for v in range(G.getV()):
+        degree = degree(G, v)
+        if degree > maxDegree:
+            maxDegree = degree
+    return maxDegree
+
+def averageDegree(G):
+    return 2.0*G.getE()/G.getV()
+
+def numberOfSelfLoops(G):
+    count = 0
+    for v in range(G.getV()):
+        for w in G.adj[v]:
+            if v==w:
+                count+=1
+    return count/2
--- Notes.md
+++ Notes.md
@@ -4,4 +4,6 @@
 
 1. Output 1:
    
-   - python3 Test.py tinyG.txt 
+   - python3 Test.py tinyG.txt
+
+2. Commit 2: implemented graph api in file Graph.py. They are not part of Graph class yet. but [Graph.java](https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/Graph.java.html) has them as class members. May be later. No output.
